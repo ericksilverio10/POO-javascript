9b879f48d591e414c9aa956a2d75058097d86fb8
Erick <erick1junior@hotmail.com>
2022-03-04 15:53:50 -0300

    Update files

---
+++ getters-setters.js
@@ -0,0 +1,29 @@
+class User {
+    set name(value){
+        if(value.length < 1){
+            throw new Error('Por favor, escolha um nome para o usuario')
+        }
+        this._name = value;
+    }
+    get name(){
+        return this._name;
+    }
+    set password(value){
+        if(value.length < 6){
+            throw new Error('A senha deve ter no mínimo 6 caracteres')
+        }
+        this._password = value;
+    }
+
+    get password(){
+        return '*'.repeat(this._password.length);
+    }
+}
+const newUser = new User()
+newUser.name = 'Erick'
+newUser.password = '123456'
+
+console.log(newUser.name)
+//Erick
+console.log(newUser.password)
+//******
